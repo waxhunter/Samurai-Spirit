e4dd40158d12cd73bac25f782eee4ab7097a21f9
Leonardo Tagliaro <leotgo@hotmail.com>
2014-06-03 18:33:05 -0300

    Hitting Enemy with sound! (some attacks hit detection are still missing though)

--- Assets/Scripts/KeiichiroAIController.cs
+++ Assets/Scripts/KeiichiroAIController.cs
@@ -1,15 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class KeiichiroAIController : MonoBehaviour {
 
+	public AudioSource audioSrc1;
+	public AudioSource audioSrc2;
 	public Animator animCtrl;
+
+	public AudioClip TakeHitFX;
+
+	public List<AudioClip> TakeHitVoices;
 
 	void OnTriggerEnter2D(Collider2D coll)
 	{
 		if(coll.gameObject.tag == "Player Hit Area")
 		{
-			animCtrl.SetBool ("Hit Taken", true);
+			if(animCtrl.GetBool ("Hit Taken") == false)
+			{
+				animCtrl.SetBool ("Hit Taken", true);
+				audioSrc1.clip = TakeHitVoices[Random.Range (0, TakeHitVoices.Count)];
+				audioSrc1.Play();
+				audioSrc2.clip = TakeHitFX;
+				audioSrc2.Play();
+			}
 		}
 	}
 
