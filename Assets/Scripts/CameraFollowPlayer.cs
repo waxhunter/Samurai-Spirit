6a5b095ebba4dc722d33fe23a3359ad8b37d8337
Leonardo Tagliaro <leotgo@hotmail.com>
2014-06-04 20:51:05 -0300

    Camera now follows player in Y axis, using deadzone value

--- Assets/Scripts/CameraFollowPlayer.cs
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -5,10 +5,20 @@ public class CameraFollowPlayer : MonoBehaviour {
 
 	public float cameraMinXBound;
 	public float cameraMaxXBound;
+
+	public float cameraYOffset;
+	public float cameraXOffset;
+
+	public float cameraYDeadZone;
+
 	public GameObject player;
 	Vector3 camtarget;
 
 	// Use this for initialization
+	void Awake() {
+
+	}
+
 	void Start () {
 	
 	}
@@ -17,7 +27,21 @@ public class CameraFollowPlayer : MonoBehaviour {
 	void Update () {
 		Vector3 campos = this.transform.position;
 		Vector3 playerpos = player.transform.position;
-		camtarget = new Vector3(playerpos.x, campos.y, campos.z);
+		camtarget = new Vector3(playerpos.x + cameraXOffset, campos.y, campos.z);
 		if(camtarget.x > cameraMinXBound && camtarget.x < cameraMaxXBound) transform.position = Vector3.Lerp(campos, camtarget, 0.05f * Vector3.Distance (campos, camtarget));
+
+		float pToCamDiff = (player.transform.position.y - transform.position.y);
+		if(Mathf.Abs (pToCamDiff) > cameraYDeadZone)
+		{
+			campos = this.transform.position;
+			if(pToCamDiff > 0)
+			{
+				transform.position = Vector3.Lerp(campos, new Vector3(campos.x, playerpos.y + (pToCamDiff - cameraYDeadZone), campos.z), 0.025f * Mathf.Abs (campos.y - playerpos.y));
+			}
+			else if(pToCamDiff < 0)
+			{
+				transform.position = Vector3.Lerp(campos, new Vector3(campos.x, playerpos.y - (pToCamDiff + cameraYDeadZone), campos.z), 0.025f * Mathf.Abs (campos.y - playerpos.y));
+			}
+		}
 	}
 }
--- Assets/Scripts/PlayerPhysics.cs
+++ Assets/Scripts/PlayerPhysics.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerPhysics : MonoBehaviour {
 
@@ -11,20 +12,26 @@ public class PlayerPhysics : MonoBehaviour {
 	void Update()
 	{
 		Vector2 collPos = this.collider2D.transform.position;
-		RaycastHit2D[] raycast = Physics2D.LinecastAll(collPos, collPos + new Vector2(0, - (this.collider2D.transform.localScale.y / 2 + groundLineCastCorrector)));
+
+		List<float> test_pos_x = new List<float>() {0, this.collider2D.transform.localScale.x/2, -this.collider2D.transform.localScale.x/2};
 
 		isOnGround = false;
-		foreach(RaycastHit2D rc in raycast)
+		for(int i=0; i<3; i++)
 		{
-			if(rc.collider != null)
-			{
-				if(rc.collider.gameObject.name != "Player")
-				{
-					isOnGround = true;
-					print (rc.collider.name);
-				}
-			}
+			RaycastHit2D[] raycast = Physics2D.LinecastAll(collPos, collPos + new Vector2(test_pos_x[i], - (this.collider2D.transform.localScale.y / 2 + groundLineCastCorrector)));
 
+			foreach(RaycastHit2D rc in raycast)
+			{
+				if(rc.collider != null)
+				{
+					if(rc.collider.gameObject.name != "Player")
+					{
+						isOnGround = true;
+						print (rc.collider.name);
+					}
+				}
+
+			}
 		}
 
 		animCtrl.SetBool("OnFloor", isOnGround);
