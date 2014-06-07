825a384444abfb1a75bbb2b9d006075ff9d03b33
Leonardo Tagliaro <leotgo@hotmail.com>
2014-06-07 10:41:39 -0300

    Enemy projectiles created (action still not implemented). Projectile base behaviour added

---
+++ Assets/ProjectileBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileBehaviour : MonoBehaviour {
+
+	Vector3 originalPosition;
+	public GameObject launcher;
+
+	PhysicsController physicsCtrl;
+
+	public float speed;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.name != "Enemy" && !other.isTrigger && !Physics2D.GetIgnoreLayerCollision(other.gameObject.layer, this.gameObject.layer))
+		{
+			//this.collider2D.enabled = false;
+			this.transform.localPosition = originalPosition;
+			this.gameObject.SetActive(false);
+			//this.collider2D.enabled = true;
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+
+	}
+
+	void Awake()
+	{
+		physicsCtrl = launcher.GetComponent<PhysicsController>();
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		originalPosition = this.transform.localPosition;;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rigidbody2D.AddForce (new Vector2(physicsCtrl.direction * speed, 0f));
+	}
+}
